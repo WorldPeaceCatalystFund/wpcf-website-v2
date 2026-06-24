42799c0e37b36b21de2dd9a59a96a1c07a67d1f4
WolrdPeaceCatalystFund <president@worldpeacecatalystfund.org>
2026-06-23 21:39:30 -0400

    added @angular/core@22 angular/@cli22

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
     selector: 'app-homepage',
     imports: [],
     templateUrl: './homepage.component.html',
+    changeDetection: ChangeDetectionStrategy.Eager,
     styleUrl: './homepage.component.css'
 })
 export class HomepageComponent {
