217abdcced1916f2c17d990842ce4f1d63b284ff
WolrdPeaceCatalystFund <president@worldpeacecatalystfund.org>
2026-06-30 21:54:46 -0400

    This is the new home page component, updated to fit with the new template.

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -3,8 +3,9 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 @Component({
     selector: 'app-homepage',
     imports: [],
+    standalone: true,
     templateUrl: './homepage.component.html',
-    changeDetection: ChangeDetectionStrategy.Eager,
+    changeDetection: ChangeDetectionStrategy.OnPush,
     styleUrl: './homepage.component.css'
 })
 export class HomepageComponent {
